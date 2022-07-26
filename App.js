63ab5426540bac3ba0067b43395e175c968d2a71
Polyana Balotin <polybalotin@hotmail.com>
2022-07-26 20:16:06 -0300

    feat: add task logic

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Tasks from './components/Tasks';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
 
@@ -9,7 +24,15 @@ export default function App() {
         <Text style={styles.sectionTitle}>Lista de Tarefas</Text>
 
         <View style={styles.items}>
-          <Tasks text={'Task 1'} />
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Tasks key={index} text={item} />
+                </TouchableOpacity>
+              ) 
+            })
+          }
         </View>
 
       </View>
@@ -18,8 +41,13 @@ export default function App() {
         behaviour={Platform.OS === 'ios' ? 'padding': 'height'}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder={'Escreva uma tarefa'} />
-        <TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder={'Escreva uma tarefa'}
+          value={task}
+          onChangeText={text => setTask(text) }
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
@@ -51,12 +79,12 @@ const styles = StyleSheet.create({
     bottom: 60,
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
   input: {
     paddingVertical: 15,
-    paddingHorizontal: 15,
+    paddingHorizontal: 25,
     backgroundColor: '#FFF',
     borderRadius: 60,
     borderColor: '#C0C0C0',
